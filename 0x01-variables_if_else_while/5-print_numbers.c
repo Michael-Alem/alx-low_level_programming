2249958578fe6a801bac6e697f0b7d5d0d1f0ba9
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-11 16:53:49 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints the task phrase
+ * Return: Return void
+ */
+int main(void)
+{
+	int c;
+	
+	for (c = 0 ; c <= 9 ; ++c)
+		printf("%d", c);
+	printf("\n");
+		return (0);
+}
