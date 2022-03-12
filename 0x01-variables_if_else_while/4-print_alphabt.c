140a9b33d8bfe1eaa2721ef211c74bd67769fef6
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-11 16:42:24 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-Main function
+ * Description -Lower case alphabet letters
+ * Return: void
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a' ; c <= 'z' ; ++c)
+		putchar(c);
+	putchar('\n')
+		return (0);
+}
