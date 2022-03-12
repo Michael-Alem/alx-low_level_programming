680bb21253c36e420a7712bf21dc77f7a39f1446
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-11 17:48:12 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints the task phrase
+ * Return: Return void
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; ++i)
+		putchar(i + '0');
+	putchar("\n");
+		return (0);
+}
