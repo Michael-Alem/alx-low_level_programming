cb83c07eb0aac6f7f998df8f3377e47c3dada273
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-11 16:17:06 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
-* main -Main function
-* Description - Lower Case alphabet letters
-* Return: Void
-*/
+ * main -Main function
+ * Description - Lower Case alphabet letters
+ * Return: Void
+ */
 int main(void)
 {
 char c;
-
 for (c = 'a'; c <= 'z'; ++c)
-putchar(c);
+	putchar(c);
 putchar('\n');
 return (0);
 }
