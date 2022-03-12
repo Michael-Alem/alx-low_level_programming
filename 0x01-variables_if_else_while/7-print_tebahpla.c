422653a8c564d765a1a35b85c3fb6d8558318317
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-11 17:55:21 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints the task phrase
+ * Return: Return void
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c >= 'a' ; --c)
+		putchar(C);
+			putchar('\n');
+			return(0)
+}
