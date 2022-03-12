02f9a4f1bf96dac23584963da998f15c69a1ada2
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-11 18:13:19 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,44 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /* more headers goes there */
 
 /**
  * main- main function
- * Description- Prints the task phrase according to the condition it meets
+ * Description- Prints the task phrase
  * Return: Returns void
  */
 
 /* betty style doc for function main goes there */
 
-int main(void)
+int main (void)
 
 {
-
-  int n;
-
-
-  srand(time(0));
-
-  n = rand() - RAND_MAX / 2;
-
-  /* your code goes there */
-  if (n > 0)
-  {
-    printf("%d is positive\n", n);
-  }
-  else
-  {
-    if (n == 0)
-    {
-      printf("%d is zero\n", n);
-    }
-    else
-    {
-      printf("%d is negative\n", n);
-    }
-  }
-  return (0);
-
+	int n;
+	int lastDigit;
+	srand(time(0));
+	n =rand() - RAND_MAX/2;
+	/* your code goes there */
+	lastDigit = n % 10;
+	if (lastDigit)
+		printf ('Last digit of %d is %d and is 0\n", n, lastDigit);
+	else
+		if (lastDigit < 6 && lastDigit ! = 0)
+			printf ("Last digit of %d and is less than 6 nd not 0\n", n, lastDigit);
+	return (0);
 }
