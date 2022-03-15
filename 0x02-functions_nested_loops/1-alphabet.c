a9ffa884662e8f78400248f52cb384a3dbc135a4
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-03-15 23:16:41 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** main - main function
+ * description - prints lowercase alphabet
+ * return: always 0(success)
+ */
+int main()
+{
+  print_alphabet();
+  putchar ('\n');
+  return (0);
+}
