3470e4891707f586a3628dc22fd7ebb5ccdf8cb6
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-15 16:08:05 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+int main(void)
+{
+	print_putchar('_');
+	print_putchar('p');
+	print_putchar('u');
+	print_putchar('t');
+	print_putchar('c');
+	print_putchar('h');
+	print_putchar('a');
+	print_putchar('r');
+	print_putchar('\n');
+
+	return (0);
+}
