6a35dfad31b4abbddd4cea43d6395f5df6c207ef
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-03-16 01:35:45 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - main function
- * Descripiton - prints putchar
- * Return - always 0 (success)
+ * Descripiton - prints alphabet
+ * Return - void
  */
 
 void print_alphabet(void)
