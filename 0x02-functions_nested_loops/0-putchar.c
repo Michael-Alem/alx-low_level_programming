d82e31d94d2d3987e61d9eccbe50acb196921505
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-15 10:49:54 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+
+void printing(char *s);
+int main()
+{
+	char s[] = "_putchar";
+	return(0);
+}
+void printing(char*s);
+{
+	int i=0;
+	while (s[i] != '\n')
+	{
+		putchar(s[i]);
+		i++;
+	}
+}
