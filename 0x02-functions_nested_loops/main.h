e237c7923b3008365e5b8e52b3550d26af5742c0
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-03-15 21:47:50 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,10 +5,18 @@
  * Return - always 0 (success)
  */
 
-int main(void)
+void print(char *s)
 {
-    char s[] = "_putchar\n";
-    
+    int i=0;
+    while (s[i] != '\0' )
+    {
+        putchar(s[i]);
+        i++;
+    }
+}
+    int main()
+{
+    char s[] = "_putchar";
     print(s);
     return (0);
-}
+    }
