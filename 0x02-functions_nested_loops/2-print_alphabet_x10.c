7a3cadbaf30755481332678298c491cfe973e0f4
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-15 16:44:46 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet times 10
+ *
+ * return 0
+ */
+void print_alphabet_x10(void)
+{
+	char i=o , j;
+
+	while (i <=10)
+	{
+		char j;
+
+		for (j = 'a' ; j <= 'z' ; j++)
+	}
+	print_alphabet_x10('\n');
+	i++;
+}
+return(0);
