1e00c1bdc9950c0267e55077b1b0756cc55d67ab
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-15 11:20:45 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,25 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
-
+/**
+ * main - main function
+ * print - prints putchar
+ * 
+ * Return: always 0(success)
+ */
 void print(char *s)
 {
-int i = 0;
-    
-    while (s[i] != '\0')     
-    {
-    putchar(s[i]);
-    i++;
-    }
+	int i = 0;
+	
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
 }
-    int main()
-
+int main()
 {
-    char s[] = "_putchar\n";
-    print(s);
-    return (0);
+	char s[] = "_putchar\n";
+	print(s);
+	return (0);
 }
