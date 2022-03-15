36f637313cf2097a687de77aaf86e0c3cc34af8e
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-03-15 21:22:54 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,22 +4,23 @@
 /**
  * main - main function
  * print - prints putchar
- * 
+ * Description - prints number of string
  * Return: always 0(success)
  */
 void print(char *s)
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		putchar(s[i]);
 		i++;
 	}
 }
-int main()
+int main(void)
 {
 	char s[] = "_putchar\n";
+	
 	print(s);
 	return (0);
 }
