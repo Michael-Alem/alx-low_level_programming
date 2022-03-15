147cad91a4552a64e80e18f2c8bff9ffbfaaebd4
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-03-15 23:50:44 +0300

    Update main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,13 +1,12 @@
 #include ,stdio.h>
 /**
- * void - prints alphabet
+ * main - check the code
  * 
+ * Return - Always 0
  */
 
-void print_alphabet(void)
+int main(void)
 {
-  char i;
-
-  for ( i = 'a'; i <= 'z'; i++)
-    putchar(i);
+  print_alphabet();
+  return (0);
 }
