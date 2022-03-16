a3edffa9da3acb898a2eaad32da6547c37482bc0
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-15 17:30:07 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Short description, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int print_sign(int c)
+
+{
+
+if (n > 0)
+{
+putchar ('+');
+return (1);
+}
+
+else if (n == 0)
+{
+return (0);
+putchar ('0');
+}
+else
+{
+return (-1);
+putchar ('-');
+
+}
+
+}
