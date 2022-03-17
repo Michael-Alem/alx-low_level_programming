a0eb29933c27712a7015d59c37dea17e052533a8
Michael-Alem <michaelalemtsehay@gmail.com>
2022-03-17 09:16:42 -0700

    task debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main -main
+ * return : 0
+ */
+
+int main (void)
+{
+	int i;
+	i=0;
+
+	positive_or_ngative(i);
+
+	return(0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stido.h>
+/**
+ * main - causes an infinite loop
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*
+	 * while (i < 10)
+	 * {
+	 * Infinite loop - no increase of variable
+	 * putchar(i);
+	 }
+	 */
+	printf("Infinite loop avoided! \\0/\n");
+	return (0);
+}
