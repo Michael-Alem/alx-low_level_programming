e39f26f52cae6abdbcf9f4f8ce4ee6fb9b23123d
Michael-Alem <99313463+Michael-Alem@users.noreply.github.com>
2022-04-05 13:12:52 +0300

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,7 +2,7 @@
 /**
  * _putchar - writes the characters of c to stdout
  * @c: the character to print
- * return: on sucess 1
+ * return: on success 1
  * on error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
